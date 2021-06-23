b591157c11d402c27de1a32b363a4291df55e85c
ts2905 <84340990+ts2905@users.noreply.github.com>
2021-06-23 17:05:08 -0400

    Updated ReadMe, made csv files viewable

--- blackjack_game.py
+++ blackjack_game.py
@@ -6,91 +6,6 @@ print("Shuffle up and deal!")
 print("----------------------------------------")
 print()
 
-#using an API get request to import 6 decks of cards
-#import requests
-#shuffleurl='https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6'
-#resp_shuffle=requests.get(shuffleurl)
-#deck=resp_shuffle.json()
-#deck_id= deck["deck_id"]
-
-#using a post API to draw from the deck
-#drawurl= f'https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count={2}'
-#resp_draw=requests.post(drawurl)
-#print(resp_draw.status_code)
-#cards_drawn=resp_draw.json()
-#print(cards_drawn)
-
-#assigning a player and a dealer
-#player()
-#dealer()
-
-#if cards_drawn["cards"][0]["value"]:
-    #print(cards_drawn["cards"][0]["value"])
-
-#inputdict[key] = newvalue
-
-#or using a dictionary defined within the code
-# deck = [
-#     {"id" : 1, "suit": 'clubs', "card": 'Ace',"value": 11},
-#     {"id" : 2, "suit": 'clubs', "card": '2', "value": 2 },
-#     {"id" : 3, "suit": 'clubs', "card": '3', "value": 3 },
-#     {"id" : 4, "suit": 'clubs', "card": '4', "value": 4 },
-#     {"id" : 5, "suit": 'clubs', "card": '5', "value": 5 },
-#     {"id" : 6, "suit": 'clubs', "card": '6', "value": 6 },
-#     {"id" : 7, "suit": 'clubs', "card": '7', "value": 7 },
-#     {"id" : 8, "suit": 'clubs', "card": '8', "value": 8 },
-#     {"id" : 9, "suit": 'clubs', "card": '9', "value": 9 },
-#     {"id" : 10, "suit": 'clubs', "card": '10', "value": 10 },
-#     {"id" : 11, "suit": 'clubs', "card": 'Jack', "value": 10 },
-#     {"id" : 12, "suit": 'clubs', "card": 'Queen', "value": 10 },
-#     {"id" : 13, "suit": 'clubs', "card": 'King', "value": 10 },
-
-#      {"id" : 14, "suit": 'diamonds', "card": 'Ace', "value": 11},
-#      {"id" : 15, "suit": 'diamonds', "card": '2', "value": 2 },
-#      {"id" : 16, "suit": 'diamonds', "card": '3', "value": 3 },
-#      {"id" : 17, "suit": 'diamonds', "card": '4', "value": 4 },
-#      {"id" : 18, "suit": 'diamonds', "card": '5', "value": 5 },
-#      {"id" : 19, "suit": 'diamonds', "card": '6', "value": 6 },
-#      {"id" : 20, "suit": 'diamonds', "card": '7', "value": 7 },
-#      {"id" : 21, "suit": 'diamonds', "card": '8', "value": 8 },
-#      {"id" : 22, "suit": 'diamonds', "card": '9', "value": 9 },
-#      {"id" : 23, "suit": 'diamonds', "card": '10', "value": 10 },
-#      {"id" : 24, "suit": 'diamonds', "card": 'Jack', "value": 10 },
-#      {"id" : 25, "suit": 'diamonds', "card": 'Queen', "value": 10 },
-#      {"id" : 26, "suit": 'diamonds', "card": 'King', "value": 10 },
-
-#      {"id" : 27, "suit": 'hearts', "card": 'Ace', "value": 11},
-#      {"id" : 28, "suit": 'hearts', "card": '2', "value": 2 },
-#      {"id" : 29, "suit": 'hearts', "card": '3', "value": 3 },
-#      {"id" : 30, "suit": 'hearts', "card": '4', "value": 4 },
-#      {"id" : 31, "suit": 'hearts', "card": '5', "value": 5 },
-#      {"id" : 32, "suit": 'hearts', "card": '6', "value": 6 },
-#      {"id" : 33, "suit": 'hearts', "card": '7', "value": 7 },
-#      {"id" : 34, "suit": 'hearts', "card": '8', "value": 8 },
-#      {"id" : 35, "suit": 'hearts', "card": '9', "value": 9 },
-#      {"id" : 36, "suit": 'hearts', "card": '10', "value": 10 },
-#      {"id" : 37, "suit": 'hearts', "card": 'Jack', "value": 10 },
-#      {"id" : 38, "suit": 'hearts', "card": 'Queen', "value": 10 },
-#      {"id" : 39, "suit": 'hearts', "card": 'King', "value": 10 },
-
-#      {"id" : 40, "suit": 'spades', "card": 'Ace', "value": 11},
-#      {"id" : 41, "suit": 'spades', "card": '2', "value": 2 },
-#      {"id" : 42, "suit": 'spades', "card": '3', "value": 3 },
-#      {"id" : 43, "suit": 'spades', "card": '4', "value": 4 },
-#      {"id" : 44, "suit": 'spades', "card": '5', "value": 5 },
-#      {"id" : 45, "suit": 'spades', "card": '6', "value": 6 },
-#      {"id" : 46, "suit": 'spades', "card": '7', "value": 7 },
-#      {"id" : 47, "suit": 'spades', "card": '8', "value": 8 },
-#      {"id" : 48, "suit": 'spades', "card": '9', "value": 9 },
-#      {"id" : 49, "suit": 'spades', "card": '10', "value": 10 },
-#      {"id" : 50, "suit": 'spades', "card": 'Jack', "value": 10 },
-#      {"id" : 51, "suit": 'spades', "card": 'Queen', "value": 10 },
-#      {"id" : 52, "suit": 'spades', "card": 'King', "value": 10 }
-# ]
-
-# active_deck = deck
-
-
 from pandas import read_csv
 
 while True:
@@ -115,7 +30,7 @@ from pprint import pprint
 player_card1 = random.choice(active_deck)
 active_deck = [i for i in active_deck if not (i["id"] == player_card1["id"])]
 print(f"       Draw 1: {player_card1['card']} of {player_card1['suit']}")
-print("Cards remaining in deck:",len(active_deck))
+# print("Cards remaining in deck:",len(active_deck))
 
 
 # Player is delt second card
