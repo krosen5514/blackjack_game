32c5a3de4b2233ba22bcae6f6583965d23465ef0
krosen5514 <84346108+krosen5514@users.noreply.github.com>
2021-06-22 21:01:27 -0400

    Code working up until user hitting or staying first time, end of class 6/22

--- blackjack_game.py
+++ blackjack_game.py
@@ -28,7 +28,7 @@ print('----------------------------------------')
 
 #or using a dictionary defined within the code
 deck=[
-    {"suit": 'clubs', "card": 'Ace',"value": [ 1, 11 ]},
+    {"suit": 'clubs', "card": 'Ace',"value": 11},
     {"suit": 'clubs', "card": '2', "value": 2 },
     {"suit": 'clubs', "card": '3', "value": 3 },
     {"suit": 'clubs', "card": '4', "value": 4 },
@@ -42,7 +42,7 @@ deck=[
     {"suit": 'clubs', "card": 'Queen', "value": 10 },
     {"suit": 'clubs', "card": 'King', "value": 10 },
 
-     {"suit": 'diamonds', "card": 'Ace', "value": [ 1, 11 ] },
+     {"suit": 'diamonds', "card": 'Ace', "value": 11},
      {"suit": 'diamonds', "card": '2', "value": 2 },
      {"suit": 'diamonds', "card": '3', "value": 3 },
      {"suit": 'diamonds', "card": '4', "value": 4 },
@@ -56,7 +56,7 @@ deck=[
      {"suit": 'diamonds', "card": 'Queen', "value": 10 },
      {"suit": 'diamonds', "card": 'King', "value": 10 },
 
-     {"suit": 'hearts', "card": 'Ace', "value": [ 1, 11 ] },
+     {"suit": 'hearts', "card": 'Ace', "value": 11},
      {"suit": 'hearts', "card": '2', "value": 2 },
      {"suit": 'hearts', "card": '3', "value": 3 },
      {"suit": 'hearts', "card": '4', "value": 4 },
@@ -70,7 +70,7 @@ deck=[
      {"suit": 'hearts', "card": 'Queen', "value": 10 },
      {"suit": 'hearts', "card": 'King', "value": 10 },
 
-     {"suit": 'spades', "card": 'Ace', "value": [ 1, 11 ] },
+     {"suit": 'spades', "card": 'Ace', "value": 11},
      {"suit": 'spades', "card": '2', "value": 2 },
      {"suit": 'spades', "card": '3', "value": 3 },
      {"suit": 'spades', "card": '4', "value": 4 },
@@ -88,10 +88,40 @@ import random
 #Point values dict
 #player_hand=()
 
+#Player drawing cards
 player_card1=random.choice(deck)
 player_card2=random.choice(deck)
 print('You Drew:', player_card1["card"], "of", player_card1["suit"], "and", player_card2["card"], "of", player_card2["suit"])
 
+#Getting an initial score for the player
 player_score1=0
-player_score1=player_card1["value"]+player_card2["value"]
-print(player_score1)
+player_score1=(player_card1["value"]+player_card2["value"])
+
+#Giving the player blackjack
+if player_score1 == 21:
+    print('Blackjack! Nice job')
+
+#Giving the dealer cards
+dealer_card1=random.choice(deck)
+dealer_card2=random.choice(deck)
+print('The Dealer Drew:', dealer_card1["card"], "of", dealer_card1["suit"])
+dealer_score1=(dealer_card1["value"]+dealer_card2["value"])
+
+#Dealer gets blackjack
+if dealer_score1 == 21:
+    print('Dealer has Blackjack... sorry...')
+
+#Hit or stay, this will be a while loop
+
+player_action= input("Would you like to HIT or STAY?")
+
+if player_action.upper() == ("HIT"):
+    player_card3=random.choice(deck)
+    print('You Drew:', player_card3["card"], "of", player_card3["suit"])
+elif player_action.upper() == ("STAY"):
+    print("Game ends")
+else:
+    print("Invalid input, try again!")
+
+player_score2=(player_card1["value"]+player_card2["value"]+player_card3["value"])
+
